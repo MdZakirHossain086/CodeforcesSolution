20ac5ffcd6609d245256cb2d9b8468a2c1c1420b
Md. Zakir Hossain <34104373+MdZakirHossain086@users.noreply.github.com>
2019-12-09 13:02:26 +0600

    Add files via upload

---
+++ 1196A_Three_Piles_of_Candies.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	long long int n,a,b,c;
+	cin>>n;
+	for(int i=0;i<n;i++)
+	{
+		cin>>a>>b>>c;
+		cout<<(a+b+c)/2<<endl;
+	}
+	return 0;
+}
